5c1b97374199a8ac18a4f697cf2995dfd6f1b555
Muchamad Nabil Ramadhani <154718315+MNR1206@users.noreply.github.com>
2024-07-22 09:10:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('sorryButton').addEventListener('click', function() {
     var message = 'Iya, aku maafkan kamu! ❤️';
-    var whatsappUrl = "https://api.whatsapp.com/send?phone=6285157590566" + '&text=' + encodeURIComponent(message);
+    var whatsappUrl = 'https://wa.me/6285157590566'  + '?text=' + encodeURIComponent(message);
 
     window.open(whatsappUrl, '_blank');
 });
