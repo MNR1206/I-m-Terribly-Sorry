4113f4649bf22d3cf2506bc920c32837014e5bef
Muchamad Nabil Ramadhani <154718315+MNR1206@users.noreply.github.com>
2024-07-22 09:02:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('sorryButton').addEventListener('click', function() {
-    var phoneNumber = '6285157590566'; 
     var message = 'Iya, aku maafkan kamu! ❤️';
     var whatsappUrl = 'https://api.whatsapp.com/send?phone=6285157590566' + phoneNumber + '&text=' + encodeURIComponent(message);
 
